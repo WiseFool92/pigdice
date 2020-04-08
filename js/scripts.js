8d9b130fe5c1fed898c5a58910b3f3e3f5f8c077
Taylor Phillips <taylorphillips133@gmail.com>
2020-04-08 13:43:13 -0700

    add input fields for names in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,33 @@
 // Business Logic ---------
 
+// Setting up the Constructor to be able to add player name and have the running score associated with that name
 function Players(name) {
   this.name = name;
-  this.currentRoundScore = currentRoundScore;
-  this.currentGameScore = currentGameScore;
-
-  name.prototype.push([])
+  this.roundScore = roundScore;
+  this.gameScore = gameScore;
 }
 
+// This constructor will hold our array to count up to 100 for each player
+function Diceroll(total) {
+  this.dice = [];
+  this.total.push(total);
+}
+
+Diceroll.prototype.addDice = function () {
+  this.roundScore = roundScore += Diceroll;
+}
 
 // function rollDice() {
 //   var die1 = document.getElementById("die1");
 //   var die2 = document.getElementById("die2");
 //   var status = document.getElementById("status");
-
 //   var d1 = Math.floor(Math.random() * 6) +1;
 //   var d2 = Math.floor(Math.random() * 6) +1;
 //   var diceTotal = d1 + d2;
 //   die1.innerHTML = d1;
 //   die2.innerHTML = d2;
 //   status.innerHTML = "You rolled "+diceTotal+"."; 
-
 // }
-
-
-
 
 // UI Logic ----------
 
