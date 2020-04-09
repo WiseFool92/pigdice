1de3bd62da76856255e3e22e026c9f7d0c8d6d00
Taylor Phillips <taylorphillips133@gmail.com>
2020-04-09 11:01:47 -0700

    change a couple variable names in js for readability

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 // Business Logic ---------
 
 // Setting up the Constructor to be able to add player name and have the running score associated with that name
-function Players(name) {
+function Player(name) {
   this.name = name;
   this.turnScore = 0;
   this.gameScore = 0;
+  this.dice = [];
 }
 
 // This constructor will hold our array to count up to 100 for each player. Start with lower number for testing purposes. 
 // Are these simply polluting our environment?
 function Score(total) {
-  this.dice = [];
   this.total.push(total);
 }
 Score.prototype.addDice = function () {
@@ -18,17 +18,17 @@ Score.prototype.addDice = function () {
 }
 
 // Sum of the totals
-Players.prototype.addDice = function () {
+Player.prototype.addDice = function (number) {
   for (var i = 0; i < this.dice.length; i++) {
 
   }
 }
 
 // Creates our players
-var playerOne = new Players("player one");
-var playerTwo = new Players("player two");
+var playerOne = new Player("player one");
+var playerTwo = new Player("player two");
 // Create our Dice... do I even need to do this...?
-var dice = new Dice();
+// var dice = new Dice();
 // function Dice(sides) {        --- Sides is never defined in our rollDice method
 //   this.sides = sides || 6;
 // }
@@ -47,16 +47,17 @@ $(document).ready(function() {
 
   // player one roll event
   $("#rolldice").on("click", function() {
-    playerOne = rollDice();
-    $("#score1").text(playerOne);
+    playerOneRoll = rollDice();
+    playerOne.addDice(playerOneRoll)
+    $("#score1").text(playerOneRoll);
   })
 
   // player two roll event
   $("#rolldice2").on("click", function() {
-    playerTwo = rollDice();
-    $("#score2").text(playerTwo);
+    playerTwoRoll = rollDice();
+    $("#score2").text(playerTwoRoll);
   })
 
-  event.preventDefault();
-  console.log(rollDice);
+  // event.preventDefault();
+  console.log(playerOneRoll);
 });
